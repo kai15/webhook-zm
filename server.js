632d6a5eb52cc0f7e8c74d841c32a064b1e358c8
Kaka <kaisakai15@gmail.com>
2022-02-18 00:56:11 +0700

    #main: initial commit

--- server.js
+++ server.js
@@ -35,4 +35,18 @@ const app = express()
 const PORT = process.env.PORT || 4000
 app.get('/', (req, res) => res.send('<h2> Hello World! </h2>'));
 app.set('connection', require('./connection'))
+app.post('/zoom-webhook', (req, res) => {
+    // let _connection = await req.app.get('connection');
+    console.log(JSON.stringify(req.body.payload));
+    // try {
+    //     _connection.query("INSERT INTO `fammi`.`zoom_participants`(zoom_id, email, name, join_time, created) \
+	// 		VALUE(?, ?, ?, ?, ?)",
+    //         [body.object, body.object, body.object, body.object, body.object])
+    // }catch (e) {
+    //     console.log('error')
+    //     console.log(e.message)
+    // }
+    res.json({"response":"ok"});
+});
+
 app.listen(PORT, () => console.log(`app listening on port ${PORT}`))
