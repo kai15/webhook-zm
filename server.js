5052e8e175b60a38e1b5ea8b8c06fa7ec230f07d
Kaka <kaisakai15@gmail.com>
2022-02-18 00:54:17 +0700

    #main: initial commit

--- server.js
+++ server.js
@@ -34,4 +34,5 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 4000
 app.get('/', (req, res) => res.send('<h2> Hello World! </h2>'));
+app.set('connection', require('./connection'))
 app.listen(PORT, () => console.log(`app listening on port ${PORT}`))
