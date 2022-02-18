8f0a2dd9cb74c9e3d512aad3365fb8ce7921d33b
Kaka <kaisakai15@gmail.com>
2022-02-18 21:16:03 +0700

    #main: initial commit

--- server.js
+++ server.js
@@ -33,6 +33,7 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 4000
+app.use(express.json());
 app.get('/', (req, res) => res.send('<h2> Hello World! </h2>'));
 app.set('connection', require('./connection'))
 app.post('/zoom-webhook', (req, res) => {
