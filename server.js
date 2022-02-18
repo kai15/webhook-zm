b541ba4b0eb61a1042fc381b3659cec6baa0bd6b
Kaka <kaisakai15@gmail.com>
2022-02-19 00:57:23 +0700

    #main: initial commit

--- server.js
+++ server.js
@@ -36,17 +36,18 @@ const PORT = process.env.PORT || 4000
 app.use(express.json());
 app.get('/', (req, res) => res.send('<h2> Hello World! </h2>'));
 app.set('connection', require('./connection'))
-app.post('/zoom-webhook', (req, res) => {
-    // let _connection = await req.app.get('connection');
-    console.log("payload =>", JSON.stringify(req.body.payload.object.participant));
-    // try {
-    //     _connection.query("INSERT INTO `fammi`.`zoom_participants`(zoom_id, email, name, join_time, created) \
-	// 		VALUE(?, ?, ?, ?, ?)",
-    //         [body.object, body.object, body.object, body.object, body.object])
-    // }catch (e) {
-    //     console.log('error')
-    //     console.log(e.message)
-    // }
+app.post('/zoom-webhook', async (req, res) => {
+    var md5 = require('md5')
+    let _connection = await req.app.get('connection');
+    console.log("payload =>", JSON.stringify(req.body.payload));
+    try {
+        _connection.query("INSERT INTO `fammi`.`zoom_participants`(id, zoom_id, email, name, join_time, created) \
+			VALUE(?, ?, ?, ?, ?, now())",
+            [md5(md5(Math.random())), req.body.payload.object.id, req.body.payload.object.participant.email, req.body.payload.object.participant.user_name, req.body.payload.object.participant.join_time])
+    }catch (e) {
+        console.log('error')
+        console.log(e.message)
+    }
     res.json({"response":"ok"});
 });
 
